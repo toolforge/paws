090cff805e8646967eca64dda107036ba9ebcd6c
YuviPanda <yuvipanda@gmail.com>
2015-12-03 18:28:31 -0800

    Cleanup temporary variables

--- singleuser/user-config.py
+++ singleuser/user-config.py
@@ -9,5 +9,10 @@ if os.path.exists(custom_path):
     with open(custom_path, 'r') as f:
         exec(compile(f.read(), custom_path, 'exec'), globals())
 
+# Clean up temp variables, since pwb issues a warning otherwise
+# to help people catch misspelt config
+del f
+del custom_path
+
 # Things that should be non-easily-overridable
 usernames['*']['*'] = os.environ['JPY_USER']
