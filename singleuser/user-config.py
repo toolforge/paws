7feabb5e712d0a88f0c55d8479352ca1a9db2f64
Kunal Mehta <legoktm@gmail.com>
2015-12-04 13:38:48 -0800

    Only delete f if it was created

--- singleuser/user-config.py
+++ singleuser/user-config.py
@@ -9,9 +9,9 @@ if os.path.exists(custom_path):
     with open(custom_path, 'r') as f:
         exec(compile(f.read(), custom_path, 'exec'), globals())
 
+    del f
 # Clean up temp variables, since pwb issues a warning otherwise
 # to help people catch misspelt config
-del f
 del custom_path
 
 # Things that should be non-easily-overridable
