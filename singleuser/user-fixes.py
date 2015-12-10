1af68d7d9b366617c319e9301eddbb0f33587404
YuviPanda <yuvipanda@gmail.com>
2015-12-10 15:31:33 -0800

    Add ability to add user-fixes.py to $HOME

---
+++ singleuser/user-fixes.py
@@ -0,0 +1,12 @@
+import os
+
+custom_path = os.path.expanduser('~/user-fixes.py')
+if os.path.exists(custom_path):
+    with open(custom_path, 'r') as f:
+        exec(compile(f.read(), custom_path, 'exec'), globals())
+
+    del f
+
+# Clean up temp variables, since pwb issues a warning otherwise
+# to help people catch misspelt config
+del custom_path
