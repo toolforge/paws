2fab0628267a991e17776663447cf17448f9e4be
YuviPanda <yuvipanda@gmail.com>
2015-12-23 00:28:47 -0800

    Add separate proxy container + RC/SVC
    Allows them to be scaled and scheduled separately! Also allows
    us to integrate nbserve into this. mmmm

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -2,6 +2,8 @@ import os
 c.Application.log_level = 'DEBUG'
 
 c.JupyterHub.authenticator_class = 'oauthenticator.mediawiki.MWOAuthenticator'
+c.JupyterHub.hub_ip = '0.0.0.0'
+c.JupyterHub.proxy_api_ip = os.environ['PROXY_PORT_8001_TCP_ADDR']
 
 c.MWOAuthenticator.client_id = '8303709369544a0b519dbb0df52fc804'
 c.MWOAuthenticator.client_secret = '5eb1c8a7d97edd72db241f8cd8ea2c6f7fd9ab39'
@@ -10,7 +12,7 @@ c.MWOAuthenticator.pass_secrets = True
 c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.KubeSpawner.kube_namespace = 'paws'
 c.KubeSpawner.kube_api_endpoint = 'https://%s' % os.environ['KUBERNETES_PORT_443_TCP_ADDR']
-c.KubeSpawner.hub_ip_connect = '%s:30000' % os.environ['PAWS_PORT_30000_TCP_ADDR']
+c.KubeSpawner.hub_ip_connect = '%s:8000' % os.environ['PAWS_PORT_8000_TCP_ADDR']
 c.KubeSpawner.kube_ca_path = False
 c.KubeSpawner.kube_token = os.environ['INSECURE_KUBE_TOKEN']
 c.KubeSpawner.singleuser_image_spec = 'yuvipanda/pawsuser:latest'
