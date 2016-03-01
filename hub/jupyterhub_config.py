0fda48ec5888c5472322d2d29920cfec3ab67ab3
YuviPanda <yuvipanda@gmail.com>
2016-03-01 14:38:34 -0800

    Use external mysql db instead of sqlite
    Bug: T128508

--- hub/jupyterhub_config.py
+++ hub/jupyterhub_config.py
@@ -4,7 +4,7 @@ c.Application.log_level = 'DEBUG'
 c.JupyterHub.authenticator_class = 'oauthenticator.mediawiki.MWOAuthenticator'
 c.JupyterHub.hub_ip = '127.0.0.1'
 c.JupyterHub.ip = '0.0.0.0'
-c.JupyterHub.proxy_cmd = '/srv/jupyterhub/bin/nchp'
+c.JupyterHub.db_url = os.environ['JPY_DB_URL']
 
 c.MWOAuthenticator.client_id = '0a73e346a40b07262b6e36bdba01cba4'
 c.MWOAuthenticator.client_secret = '99b284730a79dd30e2c35988be42708ef7e57122'
