175f6f13d588ee28f33509b31807745ff6472223
yuvipanda <yuvipanda@gmail.com>
2017-08-02 13:15:25 -0700

    Add a build action to the script too

--- build.py
+++ build.py
@@ -9,23 +9,42 @@ def last_git_modified(path):
         'git',
         'log',
         '-n', '1',
-        '--pretty=format:%H',
+        '--pretty=format:%h',
         path
     ]).decode('utf-8')
 
 def build_images(prefix, images, push=False):
     for image in images:
-        image_name = prefix + image
         image_path = os.path.join('images', image)
         tag = last_git_modified(image_path)
-        image_name = '{}:{}'.format(image_name, tag[:6])
+        image_spec = '{}{}:{}'.format(prefix, image, tag)
         subprocess.check_call([
-            'docker', 'build', '-t', image_name, image_path
+            'docker', 'build', '-t', image_spec, image_path
         ])
         if push:
             subprocess.check_call([
-                'docker', 'push', image_name
+                'docker', 'push', image_spec
             ])
+
+def deploy(prefix, images):
+    image_map = {
+        'singleuser': 'jupyterhub.singleuser.image',
+        'db-proxy': 'dbProxy.image',
+        'query-killer': 'query-killer.image'
+    }
+
+    args = []
+
+    for image in images:
+        image_path = os.path.join('images', image)
+        image_name = prefix + image
+        tag = last_git_modified(image_path)
+        args.append('--set={}.name={}'.format(image_map[image], image_name))
+        args.append('--set={}.tag={}'.format(image_map[image], tag))
+
+    print(' '.join(
+        ['helm', 'upgrade', 'paws-prod', 'paws/', '-f', 'secrets.yaml'] + args
+    ))
 
 
 def main():
@@ -39,9 +58,17 @@ def main():
         action='store_true'
     )
 
+    argparser.add_argument(
+        'action',
+        choices={'build', 'deploy'}
+    )
+
     args = argparser.parse_args()
 
-    build_images(args.image_prefix, ['singleuser'], args.push)
+    images = ['singleuser', 'db-proxy', 'query-killer']
+    if args.action == 'build':
+        build_images(args.image_prefix, images, args.push)
+    else:
+        deploy(args.image_prefix, images)
 
 main()
-
