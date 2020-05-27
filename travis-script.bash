b68483cdfd60097ab2fc715c5626acd1fa6c924b
Chico Venancio <chico.venancio@bmc-group.co.jp>
2020-05-26 21:33:21 -0300

    Travis config and script: updating secrets and repo endpoint for move.

--- travis-script.bash
+++ travis-script.bash
@@ -23,7 +23,7 @@ elif [[ ${ACTION} == 'deploy' ]]; then
         -d crypt-key="${GIT_CRYPT_KEY}" \
         -d release=prod \
         -d commit=${TRAVIS_COMMIT} \
-        -d repo=https://github.com/yuvipanda/paws \
+        -d repo=https://github.com/toolforge/paws \
         -H "Authorization: Bearer ${DEPLOY_HOOK_KEY}" \
         https://paws-deploy-hook.tools.wmflabs.org/deploy
 fi
