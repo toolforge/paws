75e54feb4077c047f87a794b1661f47a84eeda29
YuviPanda <yuvipanda@gmail.com>
2021-05-22 22:52:25 +0530

    Run black on our config file

--- images/singleuser/jupyter_notebook_config.py
+++ images/singleuser/jupyter_notebook_config.py
@@ -2,17 +2,25 @@
 # flake8: noqa
 import os
 
-openrefine_path = os.path.join(os.environ['OPENREFINE_DIR'], 'refine')
-openrefine_logo_path = os.path.join(os.environ['OPENREFINE_DIR'], 'openrefine-logo.svg')
+openrefine_path = os.path.join(os.environ["OPENREFINE_DIR"], "refine")
+openrefine_logo_path = os.path.join(
+    os.environ["OPENREFINE_DIR"], "openrefine-logo.svg"
+)
 c.ServerProxy.servers = {
-    'openrefine': {
-        'command': [openrefine_path, '-p', '{port}', '-d', os.path.expanduser('~')],
-        'port': 3333,
-        'timeout': 120,
-        'launcher_entry': {
-            'enabled': True,
-            'icon_path': openrefine_logo_path,
-            'title': 'OpenRefine',
+    "openrefine": {
+        "command": [
+            openrefine_path,
+            "-p",
+            "{port}",
+            "-d",
+            os.path.expanduser("~"),
+        ],
+        "port": 3333,
+        "timeout": 120,
+        "launcher_entry": {
+            "enabled": True,
+            "icon_path": openrefine_logo_path,
+            "title": "OpenRefine",
         },
     },
 }
