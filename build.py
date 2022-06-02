0cb9c492b05717cc31282f7d8d970392a30fdd94
Vivian Rook <vrook@wikimedia.org>
2022-06-02 13:39:18 -0400

    remove db-proxy

--- build.py
+++ build.py
@@ -76,7 +76,6 @@ def deploy(prefix, images, release, install):
     image_map = {
         "paws-hub": "jupyterhub.hub.image",
         "singleuser": "jupyterhub.singleuser.image",
-        "db-proxy": "dbProxy.image",
         "deploy-hook": "deployHook.image",
     }
 
@@ -144,7 +143,7 @@ def main():
 
     args = argparser.parse_args()
 
-    images = ["paws-hub", "singleuser", "db-proxy", "deploy-hook"]
+    images = ["paws-hub", "singleuser", "deploy-hook"]
     if args.action == "build":
         build_images(args.image_prefix, images, args.commit_range, args.push)
     else:
