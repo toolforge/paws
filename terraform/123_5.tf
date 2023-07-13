db0d562777aabcc05ae28a9bd83a06596730b8a1
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2023-07-13 10:06:02 -0400

    terraform formatter (#303)
    * terraform formatter
    
    Bug: T341784
    
    * formatted

--- terraform/123_5.tf
+++ terraform/123_5.tf
@@ -2,7 +2,7 @@ resource "openstack_containerinfra_cluster_v1" "k8s_123_5" {
   name                = "paws${var.name[var.datacenter]}-123-5"
   cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_5.id
   master_count        = 1
-  node_count          = "${var.workers[var.datacenter]}"
+  node_count          = var.workers[var.datacenter]
 }
 
 resource "openstack_containerinfra_clustertemplate_v1" "template_123_5" {
@@ -10,14 +10,14 @@ resource "openstack_containerinfra_clustertemplate_v1" "template_123_5" {
   coe                   = "kubernetes"
   dns_nameserver        = "8.8.8.8"
   docker_storage_driver = "overlay2"
-  docker_volume_size    = "${var.volume_size[var.datacenter]}"
-  external_network_id   = "${var.external_network_id[var.datacenter]}"
-  fixed_subnet          = "${var.fixed_subnet[var.datacenter]}"
-  fixed_network         = "${var.fixed_network[var.datacenter]}"
-  flavor                = "${var.worker_flavor[var.datacenter]}"
+  docker_volume_size    = var.volume_size[var.datacenter]
+  external_network_id   = var.external_network_id[var.datacenter]
+  fixed_subnet          = var.fixed_subnet[var.datacenter]
+  fixed_network         = var.fixed_network[var.datacenter]
+  flavor                = var.worker_flavor[var.datacenter]
   floating_ip_enabled   = "false"
-  image                 = "${var.image_name[var.datacenter]}"
-  master_flavor         = "${var.control_flavor[var.datacenter]}"
+  image                 = var.image_name[var.datacenter]
+  master_flavor         = var.control_flavor[var.datacenter]
   network_driver        = "flannel"
 
   labels = {
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 }
 
 provider "openstack" {
-  auth_url                      = "${var.auth-url[var.datacenter]}"
-  tenant_id                     = "${var.tenant_id[var.datacenter]}"
-  application_credential_id     = "${var.application_credential_id[var.datacenter]}"
-  application_credential_secret = "${var.application_credential_secret[var.datacenter]}"
+  auth_url                      = var.auth-url[var.datacenter]
+  tenant_id                     = var.tenant_id[var.datacenter]
+  application_credential_id     = var.application_credential_id[var.datacenter]
+  application_credential_secret = var.application_credential_secret[var.datacenter]
 }
