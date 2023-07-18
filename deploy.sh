cbb2f48b96e08f02279446758094c7822d6368da
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2023-07-18 12:26:26 -0400

    Deploy via ansible (#304)
    Bug: T341785

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+if [ "${1}" = 'eqiad1' ]
+then
+  datacenter=${1}
+elif [ "${1}" = 'codfw1dev' ]
+then
+  datacenter=${1}
+else
+  echo "Please enter datacenter."
+  echo "Usage:"
+  echo "${0} <eqiad1|codfw1dev>"
+  exit
+fi
+
+
+if ! command -v kubectl ; then
+  echo "please install kubectl"
+  exit 1
+fi
+
+if ! command -v helm ; then
+  echo "please install helm"
+  exit 1
+fi
+
+if ! command -v terraform ; then
+  echo "please install terraform"
+  exit 1
+fi
+
+python3 -m venv .venv/deploy
+source .venv/deploy/bin/activate
+pip install ansible==8.1.0 kubernetes==26.1.0
+
+
+cd terraform
+terraform init
+terraform apply -var datacenter=${datacenter}  # -auto-approve
+export KUBECONFIG=$(pwd)/kube.config
+
+cd ../ansible
+ansible-playbook paws.yaml --extra-vars "datacenter=${datacenter}"
