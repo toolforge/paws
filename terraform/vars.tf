f24bbdc1465bbdfc2a795a6071fecfdd347b1d7b
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2023-07-26 12:56:51 -0400

    Remove unused terraform variables (#313)
    Bug: T342443

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -91,34 +91,3 @@ variable "workers" {
     "eqiad1"    = "3"
   }
 }
-
-
-# trove vars
-variable "network_uuid" {
-  type = map(any)
-  default = {
-    "codfw1dev" = "05a5494a-184f-4d5c-9e98-77ae61c56daa" # lan-flat-cloudinstances2b
-    "eqiad1"    = "7425e328-560c-4f00-8e99-706f3fb90bb4" # lan-flat-cloudinstances2b
-  }
-}
-variable "db_flavor_uuid" {
-  type = map(any)
-  default = {
-    "codfw1dev" = "5b2ca632-2ea0-4007-9b40-4f84f8e2428b"
-    "eqiad1"    = "55d5d90f-c5c6-44ff-bb8a-be7b077481cf"
-  }
-}
-variable "region" {
-  type = map(any)
-  default = {
-    "codfw1dev" = "codfw1dev-r"
-    "eqiad1"    = "eqiad1-r"
-  }
-}
-variable "db_size" {
-  type = map(any)
-  default = {
-    "codfw1dev" = "1"
-    "eqiad1"    = "4"
-  }
-}
