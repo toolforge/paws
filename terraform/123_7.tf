57f68196cafe16eec96140e227f6d6a0dab54bb1
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2023-09-27 09:54:00 -0400

    New cluster (#333)
    * New cluster
    
    Bug: T347111
    
    * removing 123-6 cluster

--- terraform/123_7.tf
+++ terraform/123_7.tf
@@ -1,17 +1,17 @@
-resource "openstack_containerinfra_cluster_v1" "k8s_123_6" {
-  name                = "paws${var.name[var.datacenter]}-123-6"
-  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_6.id
+resource "openstack_containerinfra_cluster_v1" "k8s_123_7" {
+  name                = "paws${var.name[var.datacenter]}-123-7"
+  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_7.id
   master_count        = 1
   node_count          = var.workers[var.datacenter]
 }
 
 resource "local_file" "kube_config" {
-  content  = resource.openstack_containerinfra_cluster_v1.k8s_123_6.kubeconfig.raw_config
+  content  = resource.openstack_containerinfra_cluster_v1.k8s_123_7.kubeconfig.raw_config
   filename = "kube.config"
 }
 
-resource "openstack_containerinfra_clustertemplate_v1" "template_123_6" {
-  name                  = "paws${var.name[var.datacenter]}-123-6"
+resource "openstack_containerinfra_clustertemplate_v1" "template_123_7" {
+  name                  = "paws${var.name[var.datacenter]}-123-7"
   coe                   = "kubernetes"
   dns_nameserver        = "8.8.8.8"
   docker_storage_driver = "overlay2"
