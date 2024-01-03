0df9b1e0332c25da422ec5bef7d87c31a0a44c34
Vivian Rook <vrook@wikimedia.org>
2024-01-03 11:51:45 -0500

    Upgrade fcos version
    Bug: T353077

--- tofu/123_11.tf
+++ tofu/123_11.tf
@@ -1,12 +1,17 @@
-resource "openstack_containerinfra_cluster_v1" "k8s_123_10" {
-  name                = "paws${var.name[var.datacenter]}-123-10"
-  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_10.id
+resource "openstack_containerinfra_cluster_v1" "k8s_123_11" {
+  name                = "paws${var.name[var.datacenter]}-123-11"
+  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_11.id
   master_count        = 1
   node_count          = var.workers[var.datacenter]
 }
 
-resource "openstack_containerinfra_clustertemplate_v1" "template_123_10" {
-  name                  = "paws${var.name[var.datacenter]}-123-10"
+resource "local_file" "kube_config" {
+  content  = resource.openstack_containerinfra_cluster_v1.k8s_123_11.kubeconfig.raw_config
+  filename = "kube.config"
+}
+
+resource "openstack_containerinfra_clustertemplate_v1" "template_123_11" {
+  name                  = "paws${var.name[var.datacenter]}-123-11"
   coe                   = "kubernetes"
   dns_nameserver        = "8.8.8.8"
   docker_storage_driver = "overlay2"
@@ -16,7 +21,7 @@ resource "openstack_containerinfra_clustertemplate_v1" "template_123_10" {
   fixed_network         = var.fixed_network[var.datacenter]
   flavor                = var.worker_flavor[var.datacenter]
   floating_ip_enabled   = "false"
-  image                 = var.image_name[var.datacenter]
+  image                 = "Fedora-CoreOS-38"
   master_flavor         = var.control_flavor[var.datacenter]
   network_driver        = "flannel"
 
