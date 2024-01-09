88b7e27bb50d0c9dc7464463e4debd36e0e0423b
Vivian Rook <vrook@wikimedia.org>
2024-01-09 12:16:22 -0500

    maybe

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,15 +1,25 @@
 terraform {
   required_version = ">= 1.6.0"
   backend "s3" {
-    #access_key                  = "7f5dc99840424406ae1e888d21b936a7"
-    bucket                      = "tofu-state"
-    endpoint                    = "https://object.eqiad1.wikimediacloud.org"
-    key                         = "paws-state"
-    region                      = "default"
-    #secret_key                  = var.ec2_credential_secret[var.datacenter]
-    skip_credentials_validation = "true"
-    skip_region_validation      = "true"
-    use_path_style              = "true"
+    region   = "eqiad1"
+    bucket   = "paws:tofu-state"
+    endpoint = "https://object.eqiad1.wikimediacloud.org"
+    key      = "state/main"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    force_path_style            = true
+
+
+    ##access_key                  = "7f5dc99840424406ae1e888d21b936a7"
+    #bucket                      = "tofu-state"
+    #endpoint                    = "https://object.eqiad1.wikimediacloud.org"
+    #key                         = "paws-state"
+    #region                      = "default"
+    ##secret_key                  = var.ec2_credential_secret[var.datacenter]
+    #skip_credentials_validation = "true"
+    #skip_region_validation      = "true"
+    #use_path_style              = "true"
   }
   required_providers {
     openstack = {
