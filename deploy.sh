bb371d69ab98608cbbbf41ac401fbe21026e14af
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2024-01-09 16:07:17 -0500

    tofu state to s3 (#354)
    * tofu state to s3
    
    Bug: T352164
    
    * maybe
    
    * command line vars
    
    * comments removed
    
    * space

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,15 @@ else
   exit
 fi
 
+if [ -n "${2}" ]
+then
+  if [ "${2}" = 'tofu' ]
+  then
+    # exit after tofu
+    tofuonly=1
+  fi
+fi
+
 
 if ! command -v kubectl ; then
   echo "please install kubectl"
@@ -31,15 +40,22 @@ if ! command -v tofu ; then
   exit 1
 fi
 
+source secrets-${datacenter}.sh
+
 python3 -m venv .venv/deploy
 source .venv/deploy/bin/activate
 pip install ansible==8.1.0 kubernetes==26.1.0
 
 
 cd tofu
-tofu init
-tofu apply -var datacenter=${datacenter}  # -auto-approve
+AWS_ACCESS_KEY_ID=${ACCESS_KEY} AWS_SECRET_ACCESS_KEY=${SECRET_KEY} tofu init
+AWS_ACCESS_KEY_ID=${ACCESS_KEY} AWS_SECRET_ACCESS_KEY=${SECRET_KEY} tofu apply -var datacenter=${datacenter} # -auto-approve
 export KUBECONFIG=$(pwd)/kube.config
+
+if [ "${tofuonly}" = '1' ]
+then
+  exit
+fi
 
 cd ../ansible
 ansible-playbook paws.yaml --extra-vars "datacenter=${datacenter}"
