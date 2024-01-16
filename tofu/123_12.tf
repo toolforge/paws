3daf8b626651a21b0916f49c2a6812e4d5e44dc2
Vivian Rook <91909295+vivian-rook@users.noreply.github.com>
2024-01-16 11:49:13 -0500

    Update paws helm chart (#365)
    * Update paws helm chart
    
    Bug: T354898
    
    * auto update of  tag
    
    ---------
    
    Co-authored-by: Github Action <auto@github.com>

--- tofu/123_12.tf
+++ tofu/123_12.tf
@@ -1,12 +1,17 @@
-resource "openstack_containerinfra_cluster_v1" "k8s_123_11" {
-  name                = "paws${var.name[var.datacenter]}-123-11"
-  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_11.id
+resource "openstack_containerinfra_cluster_v1" "k8s_123_12" {
+  name                = "paws${var.name[var.datacenter]}-123-12"
+  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_123_12.id
   master_count        = 1
   node_count          = var.workers[var.datacenter]
 }
 
-resource "openstack_containerinfra_clustertemplate_v1" "template_123_11" {
-  name                  = "paws${var.name[var.datacenter]}-123-11"
+resource "local_file" "kube_config" {
+  content  = resource.openstack_containerinfra_cluster_v1.k8s_123_12.kubeconfig.raw_config
+  filename = "kube.config"
+}
+
+resource "openstack_containerinfra_clustertemplate_v1" "template_123_12" {
+  name                  = "paws${var.name[var.datacenter]}-123-12"
   coe                   = "kubernetes"
   dns_nameserver        = "8.8.8.8"
   docker_storage_driver = "overlay2"
