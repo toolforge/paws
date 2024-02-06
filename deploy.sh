3ab44a69e5b218f2e7c1dd5abddd0fb73af79f73
Vivian Rook <vrook@wikimedia.org>
2024-02-06 08:03:10 -0500

    Restore prometheus
    Bug: T356769

--- deploy.sh
+++ deploy.sh
@@ -42,6 +42,9 @@ fi
 
 source secrets-${datacenter}.sh
 
+# save current kube.config in case we need to transfer prometheus data
+cp $(pwd)/tofu/kube.config /tmp/kube.config.previous || true # if it isn't there just keep going
+
 python3 -m venv .venv/deploy
 source .venv/deploy/bin/activate
 pip install ansible==8.1.0 kubernetes==26.1.0
@@ -51,6 +54,7 @@ cd tofu
 AWS_ACCESS_KEY_ID=${ACCESS_KEY} AWS_SECRET_ACCESS_KEY=${SECRET_KEY} tofu init -backend-config=${datacenter}-backend.conf
 AWS_ACCESS_KEY_ID=${ACCESS_KEY} AWS_SECRET_ACCESS_KEY=${SECRET_KEY} tofu apply -var datacenter=${datacenter}
 export KUBECONFIG=$(pwd)/kube.config
+cp $(pwd)/kube.config /tmp/kube.config.current
 
 if [ "${tofuonly}" = '1' ]
 then
