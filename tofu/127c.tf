6fa3cb53ac712e75978196111ca13160006da4db
Andrew Bogott <Andrewbogott@gmail.com>
2025-09-18 14:44:07 -0500

    Prepare new PAWS deployment, this time with ssh keys (#498)
    Bug: T405023
    
    Co-authored-by: Andrew Bogott <abogott@wikimedia.org>

--- tofu/127c.tf
+++ tofu/127c.tf
@@ -1,12 +1,17 @@
-resource "openstack_containerinfra_cluster_v1" "k8s_127b" {
-  name                = "paws${var.name[var.datacenter]}-127b"
-  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_127b.id
+resource "openstack_containerinfra_cluster_v1" "k8s_127c" {
+  name                = "paws${var.name[var.datacenter]}-127c"
+  cluster_template_id = resource.openstack_containerinfra_clustertemplate_v1.template_127c.id
   master_count        = 1
   node_count          = var.workers[var.datacenter]
 }
 
-resource "openstack_containerinfra_clustertemplate_v1" "template_127b" {
-  name                  = "paws${var.name[var.datacenter]}-127b"
+resource "local_file" "kube_config" {
+  content  = resource.openstack_containerinfra_cluster_v1.k8s_127c.kubeconfig.raw_config
+  filename = "kube.config"
+}
+
+resource "openstack_containerinfra_clustertemplate_v1" "template_127c" {
+  name                  = "paws${var.name[var.datacenter]}-127c"
   coe                   = "kubernetes"
   dns_nameserver        = "8.8.8.8"
   docker_storage_driver = "overlay2"
@@ -17,6 +22,7 @@ resource "openstack_containerinfra_clustertemplate_v1" "template_127b" {
   flavor                = var.worker_flavor[var.datacenter]
   floating_ip_enabled   = "false"
   image                 = "Fedora-CoreOS-38"
+  keypair_id            = "paws-magnum-vm"
   master_flavor         = var.control_flavor[var.datacenter]
   network_driver        = "calico"
 
